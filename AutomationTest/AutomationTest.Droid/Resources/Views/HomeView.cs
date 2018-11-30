af279694908fcc585953bc1a14f0e8a142cbf666
krzysztofstepnikowski <skrzychu7@gmail.com>
2018-11-30 11:14:30 +0100

    Tried to integration with App Center.

--- AutomationTest/AutomationTest.Droid/Resources/Views/HomeView.cs
+++ AutomationTest/AutomationTest.Droid/Resources/Views/HomeView.cs
@@ -13,6 +13,7 @@ namespace AutomationTest.Droid.Resources.Views
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.main);
+            //AppCenter.Start("d07c90c9-8d1a-48e7-b165-e297d7c793c6", typeof(Analytics), typeof(Crashes));
         }
     }
 }
